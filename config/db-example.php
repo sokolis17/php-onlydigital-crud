1188ee30751431a85070ca11fb02eab9e5cdb582
Igor <rononi@bk.ru>
2026-01-03 13:02:23 +0300

    Реализовал покдлючение к БД !

---
+++ config/db-example.php
@@ -0,0 +1,21 @@
+<?php
+
+$host = '127.0.0.1';
+$port = '5432';
+$db   = 'YOUR_DB';
+$user = 'YOUR_USER';
+$pass = 'YOUR_PASS';
+
+$dsn = "pgsql:host=$host;port=$port;dbname=$db";
+
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+
+try {
+    $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (\PDOException $e) {
+    throw new \PDOException($e->getMessage(), (int)$e->getCode());
+}
