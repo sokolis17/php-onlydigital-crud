93eab57b14bd7c4a258342cb75afb171363d253d
Igor <rononi@bk.ru>
2026-01-03 21:24:00 +0300

    Реализован вывод ошибок в форме

--- views/auth/register-form.php
+++ views/auth/register-form.php
@@ -5,9 +5,9 @@
                 <h4 class="card-title text-center mb-0">Регистрация</h4>
             </div>
             <div class="card-body p-4">
-                
-                <form action="/src/actions/reg.php" method="POST">
-                    
+
+                <form action="src/actions/reg.php" method="POST">
+
                     <div class="mb-3">
                         <label for="name" class="form-label">Ваше имя</label>
                         <input type="text" class="form-control" id="name" name="user_name" placeholder="Иван Иванов" required>
@@ -21,24 +21,32 @@
                     <div class="mb-3">
                         <label for="email" class="form-label">Электронная почта</label>
                         <input type="email" class="form-control" id="email" name="email" placeholder="name@example.com" required>
+                        <?php if (isset($_SESSION['error']['email'])): ?>
+                            <div class="text-danger small mt-1"><?= $_SESSION['error']['email'] ?></div>
+                            <?php unset($_SESSION['error']['email']); ?>
+                        <?php endif; ?>
                     </div>
 
                     <div class="mb-3">
                         <label for="pass" class="form-label">Пароль</label>
                         <input type="password" class="form-control" id="pass" name="pass" placeholder="******" required>
+                        <?php if (isset($_SESSION['error']['pass'])): ?>
+                            <div class="text-danger small mt-1"><?= $_SESSION['error']['pass'] ?></div>
+                            <?php unset($_SESSION['error']['pass']); ?>
+                        <?php endif; ?>
                     </div>
 
                     <div class="mb-3">
                         <label for="pass2" class="form-label">Повторите пароль</label>
-                        <input type="password" class="form-control" id="pass2" name="second_pass" placeholder="******" required>
+                        <input type="password" class="form-control" id="pass2" name="pass2" placeholder="******" required>
                     </div>
 
                     <div class="d-grid gap-2 mt-4">
                         <button type="submit" class="btn btn-primary">Создать аккаунт</button>
                     </div>
-                    
+
                 </form>
-                
+
             </div>
         </div>
     </div>
