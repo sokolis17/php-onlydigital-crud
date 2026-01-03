a4463289f329dc9b6be2610735a7e4445444f797
Igor <rononi@bk.ru>
2026-01-03 23:03:31 +0300

    Реализована обработка всех полей и вывод глобальной ошибки базы

--- src/actions/reg.php
+++ src/actions/reg.php
@@ -9,25 +9,36 @@ $tel = $_POST['tel'];
 $pass = $_POST['pass'];
 $pass2 = $_POST['pass2'];
 
-//ПАРОЛЬ
-$hash = pass_handle($pass, $pass2);
-if ($hash === false) {
-    $_SESSION['error']['pass'] = 'Пароли должны совпадать';
-    header('Location: /php-onlydigital-crud/register.php');
-}
 //ПОЧТА
-if (!uniqe_email($email)) {
+if (!uniqeEmail($email)) {
     $_SESSION['error']['email'] = 'Пользователь с такой почтой уже существует';
     header('Location: /php-onlydigital-crud/register.php');
 };
 //ТЕЛЕФОН
-if (!uniqe_phone($tel)) {
+if (!uniqePhone($tel)) {
     $_SESSION['error']['tel'] = 'Пользователь с таким телефоном уже существует';
     header('Location: /php-onlydigital-crud/register.php');
 }
+//ПАРОЛЬ
+$hash = passHandle($pass, $pass2);
+if ($hash === false) {
+    $_SESSION['error']['pass'] = 'Пароли должны совпадать';
+    header('Location: /php-onlydigital-crud/register.php');
+}
 
-if(!empty($_SESSION['error'])){
+if (!empty($_SESSION['error'])) {
     header('Location: /php-onlydigital-crud/register.php');
     exit;
 }
-?>
+
+try {
+    addUser($name, $email, $tel, $hash);
+    header('Location: /php-onlydigital-crud/login.php');
+    exit;
+} catch (PDOException $e) {
+    $_SESSION['error']['global'] = "Ошибка при регистрации: " . $e->getMessage();
+    header('Location: /php-onlydigital-crud/register.php');
+    exit;
+}
+
+
